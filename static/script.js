2476dcfec085dff351dad0a5735fe4dcca531552
sofiadenice <10soffygo@gmail.com>
2021-03-09 15:32:18 -0600

    Esquema proyecto

---
+++ static/script.js
@@ -0,0 +1,30 @@
+
+//Funcion para mostrar contenido de pestañas de HTML
+function hideDivById(divId) {
+    hideAllDivW3Includes()
+    var element = document.getElementById(divId)
+    if (element.style.display === "none") {
+        element.style.display = "block"
+    }
+}
+
+//Ocultar todo el contenido de pestañas de HTML
+function hideAllDivW3Includes() {
+    var elementArray = document.getElementsByName("pages")
+    for (var element of elementArray) {
+        element.style.display = "none"
+    }
+}
+
+w3.includeHTML()
+
+//Enviar Correo
+
+function sendEmail() {
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var message = document.getElementById("message").value
+
+    alert("De: " + name + " <" + email + "> " + "\n Mensaje: \n " + message)
+
+}
