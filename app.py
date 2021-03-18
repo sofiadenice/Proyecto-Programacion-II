3f17f47b75d941b1dff6eb07f31b66b9cdd2b0b5
Gelm20 <gilelopez@hotmail.com>
2021-03-17 18:11:46 -0600

    login agregado

--- app.py
+++ app.py
@@ -36,5 +36,20 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("registration.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,4 +1,93 @@
 
+
+/*
+************* register functionality begin
+*/
+
+function registerNewUser() {
+    var reg_user = document.getElementById("user_reg").value;
+    var reg_password = document.getElementById("passw_reg").value;
+    var reg_role = "client";
+
+    //alert(reg_user);
+    var userArray = [];
+
+    if (localStorage.getItem("lUserArray") !== null) {
+        userArray = JSON.parse(localStorage.getItem("lUserArray"));
+    }
+
+    var current_reg = {
+        user: reg_user,
+        password: reg_password,
+        role: reg_role
+    };
+
+    userArray.push(current_reg);
+
+    localStorage.setItem("lUserArray", JSON.stringify(userArray));
+
+    window.location.href = "http://127.0.0.1:5000/login"
+    //window.location.href = "http://heroku:5000/login";
+}
+
+/*
+************* login functionality begin
+*/
+function checkLogin() {
+
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("passw").value;
+
+    var userArray = JSON.parse(localStorage.getItem("lUserArray"));
+
+    if (user !== null && user !== "") {
+        if (password !== null && password !== "") {
+
+            var canLogin = checkLoginInfo(user, password, userArray);
+            if (canLogin === true) {
+                //need a method to get the role and send it into createSessionUser below
+                var role = getUserRole(user, password, userArray)
+                createSessionUser(user, password, role)
+                window.location.href = "http://127.0.0.1:5000/login";
+                //window.location.href = "http://heroku:5000/dashboard";
+            } else {
+                alert("user or password are not correct");
+            }
+
+        } else {
+            alert("password must not be empty");
+        }
+    } else {
+        alert("user must not be empty");
+    }
+
+}
+
+function checkLoginInfo(user, password, userArray) {
+    if (userArray !== null && userArray.length > 0) {
+        for (var i = 0; i < userArray.length; i++) {
+            if (userArray[i].user === user && userArray[i].password === password) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getUserRole(pUser, pPassword, pUserArray) {
+    var role = ""
+    if (pUserArray !== null && pUserArray.length > 0) {
+        var length = pUserArray.length
+        for (var i = 0; i < length; i++) {
+            if (pUserArray[i].user === pUser && pUserArray[i].password === pPassword) {
+                role = pUserArray[i].role
+                break
+            }
+        }
+    }
+    return role
+}
+
 //Funcion para mostrar contenido de pestañas de HTML
 function hideDivById(divId) {
     hideAllDivW3Includes()
